0ae3a0e0b97828ccd81d1bbb55e6a4bfbba2102e
marcmedina01 <m.h.medina@gmail.com>
2024-06-08 20:25:49 +0800

    added cors fix

--- app.js
+++ app.js
@@ -9,18 +9,21 @@ const PORT = process.env.PORT || 5001;
 
 const server = require("./routes/cal");
 
-  const app = express();
+const app = express();
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
-  //for accepting json in body
-  app.use(bodyParser.json());
+//for accepting json in body
+app.use(bodyParser.json());
 
-  app.use(`/api`, server);
+app.use(`/api`, server);
 
 
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
+app.listen(PORT, function () {
+console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+});
 
